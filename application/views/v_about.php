2474292001750ad4f8c9be575ffbb424aadae2cb
Ramdhani11 <anwarramdhanie@gmail.com>
2021-10-17 21:25:19 +0700

    Menambahkan view about.php

---
+++ application/views/v_about.php
@@ -0,0 +1,26 @@
+<section>
+    <h1><?= $judul; ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Anwar</li>
+        <li>Nama Belakang : Ramdhani</li>
+    </ul>
+    <br>
+
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>
+            jalan satapak no 12 grugul
+        </li>
+    </ul>
+    <br>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Cileduk</li>
+    </ul>
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Volley Ball</li>
+    </ul>
+</section>
