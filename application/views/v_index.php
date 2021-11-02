21560d9cb14d8eb0f66d28fb283997c22a35ba73
Ramdhani11 <anwarramdhanie@gmail.com>
2021-11-02 14:13:53 +0700

    Mengedit v_index.php

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -1,14 +1,24 @@
 <section>
     <h1><?= $judul; ?></h1>
-    <p align='justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa sapiente ipsam aperiam dignissimos impedit vero nihil exercitationem porro quidem est? Possimus voluptatum enim hic veniam amet veritatis impedit animi nemo?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero quod voluptate quos sunt deserunt reprehenderit fugiat aut, consequatur modi corporis quia velit ipsam eaque accusamus voluptates, alias a laboriosam dolorem!</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta non possimus odit molestiae officiis doloribus officia quas dolorum doloremque voluptas unde porro repellat dolor qui rem, minima illum voluptates pariatur?</p>
+    <p align='justify'>Pada pengertian codeigniter di atas
+        tadi di jelaskan bahwa codeigniter menggunakan metode MVC. Apa itu MVC? Kita juga harus mengetahui apa itu MVC sebelum masuk dan lebih jauh dalam belajar codeigniter.</p>
+    <p>MVC adalah teknik atau konsep yang memisahkan
+        komponen utama menjadi tiga komponen yaitu model, view dan
+        controller.</p>
+
     <ol type="a">
         <li>Model</li>
-        <p align="justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam vel ratione tempora, voluptates omnis culpa necessitatibus similique laboriosam possimus nostrum iste natus vitae corrupti! Magnam sunt totam vero ipsa autem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur rem dolores tempora repellat. Sit ullam nemo explicabo! Eaque magnam maxime vero facilis veniam eos repudiandae, odio, exercitationem inventore libero quisquam. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam labore officiis maxime molestiae blanditiis vel, qui reprehenderit placeat in enim illum. Optio accusamus vel, ipsa sequi maiores dolores itaque! Molestias?</p>
+        <p align="justify">Model adalah kelas yang merepresentasikan atau memodelkan tipe data yang akan digunakan oleh aplikasi. Model juga dapat didefinisakn sebagai bagian penanganan yang berhubungan dengan pengolahan atau manipulasi database. Seperti misalnya mengambil data dari database, menginput dan pengolahan database lainnya. Semua intruksi atau fungsi yang berhubung dengan pengolahan database di letakkan di dalam model. Sebagai contoh, jika ingin membuat aplikasi untuk menghitung luas dan keliling lingkaran, maka dapat memodelkan objek lingkaran sebagai kelas model.</p>
+        <p align=’justify’>Sebagai catatan, Semua model harus disimpan di dalam folder application\models</p>
         <li>View</li>
-        <p align="justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus harum, praesentium cum maxime labore ipsam amet, magnam quasi qui dolor exercitationem est doloremque reprehenderit, nobis illo quos corporis placeat ea!Loremlorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet sint quas cum ab consequatur debitis et aut doloribus praesentium. Placeat odio veritatis beatae tempora quia natus suscipit. Voluptate, deleniti iusto?Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur explicabo sint aut reprehenderit inventore perspiciatis adipisci officia voluptatum tenetur sit cumque rem natus exercitationem doloribus unde laudantium, saepe nam incidunt.</p>
+        <p align="justify">View merupakan bagian yang menangani halaman user interface atau halaman yang muncul pada user(pada browser). Tampilan dari user interface di kumpulkan pada view untuk memisahkannya dengan controller dan model sehingga memudahkan web designer dalam melakukan pengembangan tampilan halaman website.</p>
         <li>Controller</li>
-        <p align="justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam perferendis minima sunt dolore, aspernatur iure cum impedit, nihil quibusdam fugiat saepe dolores beatae, perspiciatis quidem reprehenderit at vitae! Iste, nemo?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, saepe? Eum iure magnam tempora recusandae debitis nulla deserunt quisquam, quidem cum tempore dolore, excepturi iste odio, laborum nemo voluptatibus error?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus distinctio quaerat similique, qui ratione enim accusantium perspiciatis deleniti, corporis excepturi reiciendis doloremque magnam amet dolores unde ducimus explicabo voluptatem assumenda.</p>
-
+        <p align="justify">Controller merupakan kumpulan intruksi aksi yang
+            menghubungkan model dan view, jadi user tidak akan berhubungan
+            dengan model secara langsung, intinya data yang tersimpan di
+            database (model) di ambil oleh controller dan kemudian controller
+            pula yang menampilkan nya ke view. Jadi controller lah yang mengolah
+            intruksi.</p>
+        <p align=’justify’>Dari penjelasan tentang model view dan controller di atas dapat di simpulkan bahwa controller sebagai penghubung view dan model. Misalnya pada aplikasi yang menampilkan data dengan menggunakan metode konsep mvc, controller memanggil intruksi pada model yang mengambil data pada database, kemudian controller yang meneruskannya pada view untuk di tampilkan. Jadi jelas sudah dan sangat mudah dalam pengembangan aplikasi dengan cara mvc ini karena web designer atau front-end developer tidak perlu lagi berhubungan dengan controller, dia hanya perlu berhubungan dengan view untuk mendesign tampilann aplikasi, karena back-end developer yang menangani bagian controller dan modelnya. Jadi pembagian tugas pun menjadi mudah dan pengembangan aplikasi dapat di lakukan dengan cepat dan terstruktur.</p>
     </ol>
 </section>
